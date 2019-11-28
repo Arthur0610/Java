ffbfdc469fdb51f9a520e7f2e5a01e52b049775b
Zhenkun Xie <zxie0018@student.monash.edu>
2019-11-28 08:45:35 +0800

    update

---
+++ Test/Sort/HeapSort.java
@@ -0,0 +1,22 @@
+public class HeapSort {
+    public static <E extends Comparable<E>> void heapSort(E[] list) {
+        Heap<E> heap = new Heap<>();
+
+        for(int i = 0; i < list.length; i++) {
+            heap.add(list[i]);
+        }
+
+        for(int i = list.length - 1; i >= 0; i--) {
+            list[i] = heap.remove();
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        heapSort(list);
+        for(int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
